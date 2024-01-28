a43ed00e036f1d26c2b6c68155bff8c30675abfc
ginhenrick <nguyenminhtuong19102003@gmail.com>
2024-01-28 22:50:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
